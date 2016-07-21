38cc667530eb9a3f8104003a67c35c7a78110718
shanqing.shi <shanqing.shi@sim.com>
2016-07-21 10:25:59 +0800

    Single Responsibility Principle

---
+++ app/src/main/java/com/shishanqing/demostudy/ImageCache.java
@@ -0,0 +1,38 @@
+package com.shishanqing.demostudy;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+/**
+ * Created by shishanqing on 16-7-21.
+ * 处理图片缓存
+ */
+public class ImageCache {
+    //图片LRU缓存
+    LruCache<String, Bitmap> mImageCache;
+
+    public ImageCache() {
+        initImageCache();
+    }
+
+    private void initImageCache() {
+        //计算可使用的最大内存
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        //取四分之一的可用内存作为缓存
+        final int cacheSize = maxMemory / 4;
+        mImageCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+            }
+        };
+    }
+
+    public void put(String url, Bitmap bitmap) {
+        mImageCache.put(url, bitmap);
+    }
+
+    public Bitmap get(String url) {
+        return mImageCache.get(url);
+    }
+}
---
+++ app/src/main/java/com/shishanqing/demostudy/ImageLoader.java
@@ -0,0 +1,57 @@
+package com.shishanqing.demostudy;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by shishanqing on 16-7-21.
+ * 图片加载类
+ */
+public class ImageLoader {
+    //图片缓存
+    ImageCache mImageCache = new ImageCache();
+    //线程池,线程数量为CPU的数量
+    ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime
+            .getRuntime().availableProcessors());
+
+    //加载图片
+    public void displayImage(final String url, final ImageView imageView) {
+        Bitmap bitmap = mImageCache.get(url);
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+        imageView.setTag(url);
+        mExecutorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = downloadImage(url);
+                if (bitmap == null){
+                    return;
+                }
+                if (imageView.getTag().equals(url)) {
+                    imageView.setImageBitmap(bitmap);
+                }
+                mImageCache.put(url, bitmap);
+            }
+        });
+    }
+
+    private Bitmap downloadImage(String imageUrl) {
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(imageUrl);
+            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            bitmap = BitmapFactory.decodeStream(conn.getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+}
